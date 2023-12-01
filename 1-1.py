c14d5b5f62bb9545d073cdc4a029d7d3ddfc93c7
jeanloupepasune <>
2023-12-01 21:12:56 +0100

    Add solution for day one

---
+++ 1-1.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+with open("1-1.input") as pb_input:
+    calibration_sum = 0
+    for line in pb_input.readlines():
+        chars = list(line)
+        firstd = 0
+        lastd = 0
+        for c in chars:
+            # quick and dirty: try to cast to an int.
+            # if it fails (example: cannot cast 'a' string litteral to int)
+            # just ignore and go on.
+            # could also import re... :-)
+            try:
+                if int(c) in range(0,10):
+                    firstd = int(c)
+                    break
+            except:
+                pass
+        for c in chars[::-1]:
+            try:
+                if int(c) in range(0,10):
+                    lastd = int(c)
+                    break
+            except:
+                pass
+        calibration_value = firstd * 10 + lastd
+        calibration_sum += calibration_value
+    print("Sum of all calibration values: ", calibration_sum)
---
+++ 1-2.py
@@ -0,0 +1,41 @@
+#!/bin/env python3
+
+# naughty elves made me import re eventually
+from re import findall as findall
+
+ugly_dict = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        # yes, it does go on
+        'eight': 8,
+        'nine': 9,
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        # ...and on!
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9
+        # lol
+        }
+
+with open("1-2.input") as pb_input:
+    calibration_sum = 0
+    for line in pb_input.readlines():
+        # beware of the overlap! lookahead (?=...) needed to deal with oneight and co.
+        numbers_list = findall(
+                r'(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))',
+                line
+                )
+        numbers_list = [ ugly_dict[n] for n in numbers_list ]
+        
+        calibration_sum += numbers_list[0] * 10 + numbers_list[-1]
+    print("Sum of all calibration values: ", calibration_sum)
