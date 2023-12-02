428d4524fa76bda1c5fc451a561e42bc83030390
jeanloupepasune <>
2023-12-02 08:46:05 +0100

    Add solution for day two

---
+++ 2-1.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+
+from re import findall as findall
+
+with open("2-1.input") as pb_input:
+    valid_game_ids_sum = 0
+    for line in pb_input.readlines():
+        line = line.strip()
+        data = line.split(':')
+        game_id = int(data[0].replace('Game ', ''))
+        data = data[1]
+        red_values = [ int(x) for x in findall(r'(\d+) red', data) ]
+        green_values = [ int(x) for x in findall(r'(\d+) green', data) ]
+        blue_values = [ int(x) for x in findall(r'(\d+) blue', data) ]
+        if (all(x<=12 for x in red_values) and
+            all(x<=13 for x in green_values) and
+            all(x<=14 for x in blue_values)):
+            # let's add our valid game id
+            valid_game_ids_sum += game_id
+    print("The sum of all valid game ids is: ", valid_game_ids_sum)
---
+++ 2-2.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+from re import findall as findall
+
+with open("2-2.input") as pb_input:
+    set_power_sum = 0
+    for line in pb_input.readlines():
+        line = line.strip()
+        data = line.split(':')[1]
+        red_values = [ int(x) for x in findall(r'(\d+) red', data) ]
+        green_values = [ int(x) for x in findall(r'(\d+) green', data) ]
+        blue_values = [ int(x) for x in findall(r'(\d+) blue', data) ]
+        set_power_sum += max(red_values) * max(blue_values) * max(green_values)
+    print("The sum of the powers of all minimum sets is: ", set_power_sum)
